aeb46eaf76a1e5febb4fb7a4ce74c2b8c54071ba
Andreas Stefl <stefl.andreas@gmail.com>
2010-11-27 21:57:59 +0100

    fix images

--- src/openOffice/html/ImageTranslator.java
+++ src/openOffice/html/ImageTranslator.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import openOffice.OpenDocumentText;
 import xml.Attribute;
@@ -30,18 +29,18 @@ public class ImageTranslator implements NodeTranslator {
 	public Node translateNode(Node source) {
 		try {
 			File imageFile = new File(source.findAttribute("href").getValue());
-			URI imageUri = new URI("file", null, imageFile.getAbsolutePath(), null);
 			
 			InputStream inputStream = documentText.getElement(imageFile.getPath());
 			
 			File tmpFile = imageCache.newImage(imageFile.getName());
+			URI tmpFileUri = tmpFile.getAbsoluteFile().toURI();
 			FileOutputStream outputStream = new FileOutputStream(tmpFile);
 			
 			pipeImage(inputStream, outputStream);
 			
 			
 			Node img = new Node("img");
-			img.addAttribute(new Attribute("src", imageUri.toString()));
+			img.addAttribute(new Attribute("src", tmpFileUri.toString()));
 			img.addAttribute(new Attribute("alt", tmpFile.getAbsolutePath()));
 			
 			String style = "";
@@ -57,8 +56,6 @@ public class ImageTranslator implements NodeTranslator {
 			
 			return img;
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
 		
