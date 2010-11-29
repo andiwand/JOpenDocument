9ad0e86a8593a085903d748ab07d8455c9723ba0
Andreas Stefl <stefl.andreas@gmail.com>
2010-11-29 12:20:26 +0100

    fix for stupid android

--- src/xml/reader/SaxDocumentReader.java
+++ src/xml/reader/SaxDocumentReader.java
@@ -64,6 +64,9 @@ public class SaxDocumentReader extends XmlDocumentReader {
 		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 			if (((qName != null) && (qName.length() == 0)) && (localName.length() != 0)) qName = localName;
 			
+			System.out.println("---");
+			System.out.println(qName);
+			System.out.println("---");
 			Node newNode = new Node(qName);
 			
 			for (int i = 0; i < attributes.getLength(); i++) {
